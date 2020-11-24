b94eac8458bdf9ef4d9083e01a7edce6b0ba5ea1
= <=>
2020-11-24 15:26:19 +0700

    done

---
+++ src/BinarySearch.java
@@ -0,0 +1,28 @@
+public class BinarySearch {
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+
+    static int binarySearch(int[] array, int key){
+        int low = 0;
+        int high = array.length - 1;
+        while (high >= low){
+            int mid = (low + high)/2;
+            if(key < mid){
+                high = mid - 1;
+            }else if(key == mid){
+                return mid;
+            }else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 2));
+        System.out.println(binarySearch(list, 11));
+        System.out.println(binarySearch(list, 79));
+        System.out.println(binarySearch(list, 1));
+        System.out.println(binarySearch(list, 5));
+        System.out.println(binarySearch(list, 80));
+    }
+}
